39f167db42589d012796621d926903d0f889e439
Matthias Hryniszak <padcom@gmail.com>
2018-03-19 18:24:09 +0100

    Initial import

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button role="button">Click me!</button>
+    <input type="text" placeholder="Enter your name" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import './button.scss'
+import './input.scss'
+
+@Component({
+})
+export default class App extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ index.js
@@ -0,0 +1,6 @@
+import './button.scss'
+import './input.scss'
+
+import App from './App.vue'
+
+new App({ el: '#app' })
